ea2df1866f9fdc90caa359bb92b95862f9cfe96f
RichardC0de <79238078+RichardC0de@users.noreply.github.com>
2022-09-13 08:45:57 +0700

    Add files via upload

---
+++ Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planen
+{
+    internal class Event
+    {
+        public string EventID { get; set; }
+    }
+}
