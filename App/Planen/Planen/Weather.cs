e18987f43eca2e4876db19191d3396ad307422d3
Fajar Rizky Widodo <79750744+fajarrw@users.noreply.github.com>
2022-11-01 15:05:13 +0700

    Add files via upload

---
+++ App/Planen/Planen/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Planen
+{
+    class Weather
+    {
+        private XmlAnyAttributeAttribute weatherAPI;
+        public XmlAnyAttributeAttribute WetaherAPI { get { return weatherAPI; } set { weatherAPI = value; } }   
+        public void getWeatherAPI(XmlAnyAttributeAttribute API) { }
+    }
+}
