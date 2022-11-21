525b53674fde8a8b8f275bbaee79b60e697ca23f
RichardC0de <richardhhh3@gmail.com>
2022-11-21 21:52:24 +0700

    last

--- Planen/UserControlDays.Designer.cs
+++ Planen/UserControlDays.Designer.cs
@@ -39,18 +39,19 @@ namespace Planen
             // 
             this.lbdays.AutoSize = true;
             this.lbdays.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbdays.Location = new System.Drawing.Point(125, 5);
+            this.lbdays.Location = new System.Drawing.Point(94, 4);
+            this.lbdays.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbdays.Name = "lbdays";
-            this.lbdays.Size = new System.Drawing.Size(27, 20);
+            this.lbdays.Size = new System.Drawing.Size(21, 15);
             this.lbdays.TabIndex = 0;
             this.lbdays.Text = "00";
             // 
             // lb_event
             // 
-            this.lb_event.Location = new System.Drawing.Point(4, 48);
-            this.lb_event.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_event.BackColor = System.Drawing.Color.Transparent;
+            this.lb_event.Location = new System.Drawing.Point(3, 39);
             this.lb_event.Name = "lb_event";
-            this.lb_event.Size = new System.Drawing.Size(148, 31);
+            this.lb_event.Size = new System.Drawing.Size(111, 22);
             this.lb_event.TabIndex = 1;
             this.lb_event.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -60,14 +61,14 @@ namespace Planen
             // 
             // UserControlDays
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.lb_event);
             this.Controls.Add(this.lbdays);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UserControlDays";
-            this.Size = new System.Drawing.Size(156, 97);
+            this.Size = new System.Drawing.Size(117, 79);
             this.Load += new System.EventHandler(this.UserControlDays_Load);
             this.Click += new System.EventHandler(this.UserControlDays_Click);
             this.ResumeLayout(false);
