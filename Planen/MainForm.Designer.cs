360e0d8ddf0ad7ab1a6d945210f687b589ac5fcd
RichardC0de <richardhhh3@gmail.com>
2022-11-21 23:45:48 +0700

    Terakhir

--- Planen/MainForm.Designer.cs
+++ Planen/MainForm.Designer.cs
@@ -41,8 +41,6 @@ namespace Planen
             this.label6 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.LBDATE = new System.Windows.Forms.Label();
-            this.btnAddEvent = new System.Windows.Forms.Button();
-            this.btnEdel = new System.Windows.Forms.Button();
             this.bt_refresh = new System.Windows.Forms.Button();
             this.btn_CA = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -53,18 +51,18 @@ namespace Planen
             // 
             this.daycontainer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("daycontainer.BackgroundImage")));
             this.daycontainer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.daycontainer.Location = new System.Drawing.Point(16, 82);
-            this.daycontainer.Margin = new System.Windows.Forms.Padding(2);
+            this.daycontainer.Location = new System.Drawing.Point(21, 101);
+            this.daycontainer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.daycontainer.Name = "daycontainer";
-            this.daycontainer.Size = new System.Drawing.Size(854, 518);
+            this.daycontainer.Size = new System.Drawing.Size(1139, 638);
             this.daycontainer.TabIndex = 0;
             // 
             // btnnext
             // 
-            this.btnnext.Location = new System.Drawing.Point(808, 616);
-            this.btnnext.Margin = new System.Windows.Forms.Padding(2);
+            this.btnnext.Location = new System.Drawing.Point(1077, 758);
+            this.btnnext.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnnext.Name = "btnnext";
-            this.btnnext.Size = new System.Drawing.Size(56, 19);
+            this.btnnext.Size = new System.Drawing.Size(75, 23);
             this.btnnext.TabIndex = 0;
             this.btnnext.Text = "Next";
             this.btnnext.UseVisualStyleBackColor = true;
@@ -72,10 +70,10 @@ namespace Planen
             // 
             // btnprev
             // 
-            this.btnprev.Location = new System.Drawing.Point(726, 616);
-            this.btnprev.Margin = new System.Windows.Forms.Padding(2);
+            this.btnprev.Location = new System.Drawing.Point(968, 758);
+            this.btnprev.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnprev.Name = "btnprev";
-            this.btnprev.Size = new System.Drawing.Size(56, 19);
+            this.btnprev.Size = new System.Drawing.Size(75, 23);
             this.btnprev.TabIndex = 1;
             this.btnprev.Text = "Prev";
             this.btnprev.UseVisualStyleBackColor = true;
@@ -86,10 +84,9 @@ namespace Planen
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(46, 49);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(61, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.Size = new System.Drawing.Size(80, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Sunday";
             // 
@@ -98,10 +95,9 @@ namespace Planen
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(166, 49);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(221, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.Size = new System.Drawing.Size(83, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Monday";
             // 
@@ -110,10 +106,9 @@ namespace Planen
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(392, 49);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(523, 60);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.Size = new System.Drawing.Size(118, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Wednesday";
             // 
@@ -122,10 +117,9 @@ namespace Planen
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(285, 49);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(380, 60);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.Size = new System.Drawing.Size(89, 25);
             this.label4.TabIndex = 4;
             this.label4.Text = "Tuesday";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -135,10 +129,9 @@ namespace Planen
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(655, 49);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(873, 60);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.Size = new System.Drawing.Size(66, 25);
             this.label5.TabIndex = 7;
             this.label5.Text = "Friday";
             // 
@@ -147,10 +140,9 @@ namespace Planen
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(524, 49);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(699, 60);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.Size = new System.Drawing.Size(95, 25);
             this.label6.TabIndex = 6;
             this.label6.Text = "Thursday";
             // 
@@ -159,10 +151,9 @@ namespace Planen
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(766, 49);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(1021, 60);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(73, 20);
+            this.label8.Size = new System.Drawing.Size(91, 25);
             this.label8.TabIndex = 8;
             this.label8.Text = "Saturday";
             // 
@@ -171,41 +162,19 @@ namespace Planen
             this.LBDATE.AutoSize = true;
             this.LBDATE.BackColor = System.Drawing.Color.Moccasin;
             this.LBDATE.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBDATE.Location = new System.Drawing.Point(377, 7);
-            this.LBDATE.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LBDATE.Location = new System.Drawing.Point(503, 9);
             this.LBDATE.Name = "LBDATE";
-            this.LBDATE.Size = new System.Drawing.Size(135, 26);
+            this.LBDATE.Size = new System.Drawing.Size(171, 32);
             this.LBDATE.TabIndex = 9;
             this.LBDATE.Text = "Month Year";
             this.LBDATE.Click += new System.EventHandler(this.LBDATE_Click);
             // 
-            // btnAddEvent
-            // 
-            this.btnAddEvent.Location = new System.Drawing.Point(16, 616);
-            this.btnAddEvent.Name = "btnAddEvent";
-            this.btnAddEvent.Size = new System.Drawing.Size(75, 19);
-            this.btnAddEvent.TabIndex = 10;
-            this.btnAddEvent.Text = "Add Event";
-            this.btnAddEvent.UseVisualStyleBackColor = true;
-            this.btnAddEvent.Click += new System.EventHandler(this.btnAddEvent_Click);
-            // 
-            // btnEdel
-            // 
-            this.btnEdel.Location = new System.Drawing.Point(113, 616);
-            this.btnEdel.Name = "btnEdel";
-            this.btnEdel.Size = new System.Drawing.Size(75, 19);
-            this.btnEdel.TabIndex = 11;
-            this.btnEdel.TabStop = false;
-            this.btnEdel.Text = "Edit/Delete";
-            this.btnEdel.UseVisualStyleBackColor = true;
-            this.btnEdel.Click += new System.EventHandler(this.btnLoad_Click);
-            // 
             // bt_refresh
             // 
-            this.bt_refresh.Location = new System.Drawing.Point(210, 616);
-            this.bt_refresh.Margin = new System.Windows.Forms.Padding(2);
+            this.bt_refresh.Location = new System.Drawing.Point(199, 758);
+            this.bt_refresh.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.bt_refresh.Name = "bt_refresh";
-            this.bt_refresh.Size = new System.Drawing.Size(56, 19);
+            this.bt_refresh.Size = new System.Drawing.Size(75, 23);
             this.bt_refresh.TabIndex = 12;
             this.bt_refresh.Text = "Refresh";
             this.bt_refresh.UseVisualStyleBackColor = true;
@@ -213,10 +182,10 @@ namespace Planen
             // 
             // btn_CA
             // 
-            this.btn_CA.Location = new System.Drawing.Point(365, 616);
-            this.btn_CA.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_CA.Location = new System.Drawing.Point(21, 758);
+            this.btn_CA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_CA.Name = "btn_CA";
-            this.btn_CA.Size = new System.Drawing.Size(104, 19);
+            this.btn_CA.Size = new System.Drawing.Size(139, 23);
             this.btn_CA.TabIndex = 13;
             this.btn_CA.Text = "Change Account";
             this.btn_CA.UseVisualStyleBackColor = true;
@@ -225,21 +194,20 @@ namespace Planen
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1, -5);
+            this.pictureBox1.Location = new System.Drawing.Point(1, -6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(884, 667);
+            this.pictureBox1.Size = new System.Drawing.Size(1179, 821);
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(879, 661);
+            this.ClientSize = new System.Drawing.Size(1172, 814);
             this.Controls.Add(this.btn_CA);
             this.Controls.Add(this.bt_refresh);
-            this.Controls.Add(this.btnEdel);
-            this.Controls.Add(this.btnAddEvent);
             this.Controls.Add(this.LBDATE);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label5);
@@ -252,7 +220,7 @@ namespace Planen
             this.Controls.Add(this.btnprev);
             this.Controls.Add(this.daycontainer);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -276,8 +244,6 @@ namespace Planen
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label LBDATE;
-        private System.Windows.Forms.Button btnAddEvent;
-        private System.Windows.Forms.Button btnEdel;
         private System.Windows.Forms.Button bt_refresh;
         private System.Windows.Forms.Button btn_CA;
         private System.Windows.Forms.PictureBox pictureBox1;
